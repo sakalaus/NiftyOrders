bf293259133c60ae09fa7c466c12e4d0243619ba
sakalaus@gmai.com <sakalaus@gmail.com>
2022-06-29 17:12:35 +0200

    OrderLines for Cart

--- app/src/main/java/com/pa/niftyorders/data/local/NiftyDataBase.kt
+++ app/src/main/java/com/pa/niftyorders/data/local/NiftyDataBase.kt
@@ -2,13 +2,16 @@ package com.pa.niftyorders.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
 import com.pa.niftyorders.data.local.dao.ProductDAO
 import com.pa.niftyorders.data.local.entities.Product
+import java.math.BigDecimal
 
-@Database(entities = [Product::class], version = 1)
+@Database(entities = [Product::class], version = 2)
 abstract class NiftyDataBase: RoomDatabase() {
     abstract val daoProduct: ProductDAO
     companion object{
         const val DATABASE_NAME = "nifty.db"
     }
 }
+
--- app/src/main/java/com/pa/niftyorders/ui/screens/shopwindow/ShopWindowState.kt
+++ app/src/main/java/com/pa/niftyorders/ui/screens/shopwindow/ShopWindowState.kt
@@ -1,12 +1,21 @@
 package com.pa.niftyorders.ui.screens.shopwindow
 
 import com.pa.niftyorders.data.local.entities.Product
+import java.math.BigDecimal
 
 data class ShopWindowState(
     val isCartOpen: Boolean = false,
     val products: List<Product> = emptyList(),
     val topProducts: List<Product> = emptyList(),
     val recommendedProducts: List<Product> = emptyList(),
-    val featuredGroups: List<Product> = emptyList()
+    val featuredGroups: List<Product> = emptyList(),
+    val productsInCart: List<OrderLine> = emptyList()
 ) {
 }
+
+data class OrderLine(
+    val product: Product,
+    val quantity: Float,
+    val price: BigDecimal,
+    val totalPrice: BigDecimal
+)
